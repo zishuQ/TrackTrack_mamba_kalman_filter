2e32599c5c79597b0b74a2a453a4cc7f9f0fdfa3
shang <shangqing_nie@163.com>
2025-10-27 18:46:17 +0800

    Update data directory paths to absolute paths for consistency across experiments

--- 1. YOLOX/exps/yolox_x_mot20_test.py
+++ 1. YOLOX/exps/yolox_x_mot20_test.py
@@ -23,7 +23,7 @@ class Exp(MyExp):
         self.no_aug_epochs = 10
         self.basic_lr_per_img = 0.001 / 64.0
         self.warmup_epochs = 1
-        self.data_dir = '../../dataset/'
+        self.data_dir = '/home/shang/datasets/'
 
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
         from yolox.data import (
